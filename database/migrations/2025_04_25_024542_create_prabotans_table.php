d2fa300fa999c6962a3c663d2102bef0a913e34d
Irfan <irfan@gmail.com>
2025-04-30 20:57:54 +0700

    Deskripsi perubahan yang dilakukan

--- database/migrations/2025_04_25_024542_create_prabotans_table.php
+++ database/migrations/2025_04_25_024542_create_prabotans_table.php
@@ -14,10 +14,9 @@ return new class extends Migration
         Schema::create('prabotans', function (Blueprint $table) {
             $table->id();
             $table->string('name');
-            $table->string('title');
-            $table->string('description');
-            $table->integer('stock');
-            $table->bigInteger('price');
+            $table->string('deskripsi');
+            $table->integer('stok');
+            $table->decimal('harga');
             $table->timestamps();
         });
     }
